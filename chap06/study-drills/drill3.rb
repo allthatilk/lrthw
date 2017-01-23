95c676ef20c1dbd477ac71fa5ad4e6db9168ff08
allthatilk <imogenkutz@hotmail.com>
2017-01-23 15:04:15 +0000

    Drill 3 complete

---
+++ chap06/study-drills/drill3.rb
@@ -0,0 +1,11 @@
+puts "Will my current budget cover this?"
+
+puts "I need to pay car insurance (£60)"
+puts "I need to buy a coach ticket (£10)"
+puts "I need to buy food (£20)"
+puts "I need to pay my phone bill (£10)"
+puts "I need to pay for travel in London (£20)"
+
+puts "Is my £100 budget enough? #{100 > 60 + 10 + 20 + 10 + 20}"
+
+puts "Drat..."
