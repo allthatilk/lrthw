52df77693709f9056b0cf4afbc3d12378a74b3ea
allthatilk <imogenkutz@hotmail.com>
2017-01-23 20:41:35 +0000

    Completed chap10

---
+++ chap10/study-drills/drill.rb
@@ -0,0 +1,32 @@
+# Prints string
+puts "Mary had a little lamb."
+# Prints string with interpolated string for 'snow'
+puts "Its fleece was white as #{'snow'}."
+# Prints string
+puts "And everywhere that Mary went."
+# Prints "." ten times
+puts "." * 10 # what'd that do? It printed the dot ten times
+# For all end variable in this chunk of code: Assigns string value to
+# a variable.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line, what's it do?
+# It prints them both on the same line because print doesn't add a
+# new line to the end of the output.
+# Prints "Cheese" on one line
+print end1 + end2 + end3 + end4 + end5 + end6
+# Prints "Burger" on the same line to make the word "CheeseBurger"
+puts end7 + end8 + end9 + end10 + end11 + end12
+# No mistakes made as corrected as I typed. Checked and found no
+# errors.
