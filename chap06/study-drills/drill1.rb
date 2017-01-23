1a9f5c6a18623ff1d2aa4c0ea48ef5a95248897f
allthatilk <imogenkutz@hotmail.com>
2017-01-23 14:43:43 +0000

    Study drills added

---
+++ chap06/study-drills/drill1.rb
@@ -0,0 +1,29 @@
+# Prints string
+puts "I will now count my chickens:"
+# Calculates number of hens
+puts "Hens #{25 + 30 / 6}"
+# Calculates number of roosters
+puts "Roosters #{100 - 25 * 3 % 4}"
+# Prints string
+puts "Now I will count the eggs:"
+# Calculates number of eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Prints string
+puts "Is it true that 3 + 2 < 5 - 7?"
+# Calculates maths from string and returns boolean as result to
+# evaluate comparison given
+puts 3 + 2 < 5 - 7
+# Prints string with evaluated calculation interpolated
+puts "What is 3 + 2? #{3 + 2}"
+# Same as comment above
+puts "What is 5 - 7? #{5 - 7}"
+# Prints string
+puts "Oh, that's why it's false."
+# Prints string
+puts "How about some more."
+# Prints string with interpolated comparison evaluated. Returns boolean
+puts "Is it greater? #{5 > -2}"
+# Same as comment above
+puts "Is it greater or equal? #{5 >= -2}"
+# Same as comment above
+puts "Is it less or equal? #{5 <= -2}"
